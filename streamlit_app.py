6cf6f44b34185eb3def38e599d1f2bf7f99b6ec2
aayushi2001 <80510950+aayushi2001@users.noreply.github.com>
2023-02-09 12:03:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -75,23 +75,36 @@ streamlit.write('The user entered ', fruit_choice)
 streamlit.stop()
 
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-   # my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_cur.execute("select * from fruit_load_list")   
-my_data_row = my_cur.fetchone()
-   # streamlit.text("Hello from Snowflake:")
-streamlit.text("The fruit load list contains:")   
-streamlit.text(my_data_row)
+# my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+# my_cur = my_cnx.cursor()
+#    # my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+# my_cur.execute("select * from fruit_load_list")   
+# my_data_row = my_cur.fetchone()
+#    # streamlit.text("Hello from Snowflake:")
+# streamlit.text("The fruit load list contains:")   
+# streamlit.text(my_data_row)
 
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_row)
+# streamlit.header("The fruit load list contains:")
+# streamlit.dataframe(my_data_row)
 
-my_cur.execute("select * from fruit_load_list") 
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains:")   
-streamlit.dataframe(my_data_rows)
+# my_cur.execute("select * from fruit_load_list") 
+# my_data_rows = my_cur.fetchall()
+# streamlit.header("The fruit load list contains:")   
+# streamlit.dataframe(my_data_rows)
 
+streamlit.header("The fruit load list contains:") 
+# Snowflake-related functions
+def get_fruit_load_list():
+   with my_cnx.cursor() as my_cur:
+      my_cur.execute("select * from fruit_load_list")
+      return my_cur.fetchall()
+   
+# add a button to load the fruit
+if streamlit.button('Get Fruit Load List'):
+   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+   my_data_rows = get_fruit_load_list()
+   streamlit.dataframe(my_data_rows)
+   
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
  
